613ce84efb3e42e790bba0a3e3f19908b3988529
andor-pierdelacabeza <andor@pierdelacabeza.com>
2020-01-18 16:04:14 +0100

    Primera prueba

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hola": "Mundo"}
+
+@app.get("/nombre/{nombre}")
+def read_item(nombre: str):
+    return {"Hola": nombre}
