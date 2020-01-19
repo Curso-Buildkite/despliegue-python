9de8ca22484d65f2b2e7b782e1546847947dd4db
andor-pierdelacabeza <andor@pierdelacabeza.com>
2020-01-20 00:52:02 +0100

    Agregar version

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def read_root():
     return {"Hola": "Mundo"}
 
 
+@app.get("/version")
+def read_root():
+    return {"Version": "1.1"}
+
+
 @app.get("/nombre/{nombre}")
 def read_item(nombre: str):
     return {"Hola": nombre}
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 """Test de app"""
 
-def main():
-  print("Todo bien :ok_hand:")
 
-if __name__ == '__main__': main()
+def main():
+    print("Todo bien :ok_hand:")
+
+
+if __name__ == "__main__":
+    main()
